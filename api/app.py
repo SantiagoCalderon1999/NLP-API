571c919a3689716eabdec00181fcf5abf46edb8c
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2023-10-25 09:46:18 +0100

    Create basic OpenAPI spec documentation

--- api/app.py
+++ api/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from routes.language_routes import language_routes
+from routes.language_routes import blueprint as language_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(language_routes)
+app.register_blueprint(language_blueprint)
 
 if __name__ == '__main__':
     app.run()
--- api/routes/language_routes.py
+++ api/routes/language_routes.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, request, jsonify
 from services.language import named_entity_recognition_service
+from flask_restx import Api, Resource
 
-language_routes = Blueprint('named_entity_recognition_route', __name__)
+blueprint = Blueprint('named_entity_recognition_route', __name__)
+language_routes = Api(blueprint)
 
-@language_routes.route('/language/ner/analyze', methods = ['POST'])
-def analyze():
-    text = request.json['text']
-    return jsonify(named_entity_recognition_service.analyze(text))
+@language_routes.route('/language/ner/analyze')
+class Language_routes(Resource):
+    
+    @language_routes.doc(responses={403: 'Not Authorized'})
+    @language_routes.doc(params={'text': 'Text that you want to analyze'})
+    def analyze():
+        text = request.json['text']
+        return jsonify(named_entity_recognition_service.analyze(text))
